e34cfc2fae3556ce680af643946c6ec7cb79ee49
ErickMolnarAlcalde <erickmolnar.alcalde@gmail.com>
2025-10-29 11:30:04 -0300

    subindo banco MYSQL com docker

---
+++ src/main/java/com/agragadorinvestiemnto/agragadorinvestimentos/AgragadorinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.agragadorinvestiemnto.agragadorinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgragadorinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgragadorinvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agragadorinvestiemnto/agragadorinvestimentos/AgragadorinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agragadorinvestiemnto.agragadorinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgragadorinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
